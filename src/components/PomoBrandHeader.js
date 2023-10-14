140fdf40d13d53aa3fb911456eb388c613d26fd7
Sushant Kadam <sushantkadam15@gmail.com>
2023-10-13 23:36:24 -0400

    fix hiding PomoBrandHeader after a certain duration is incorrect. Timer is now properly set for 2550 milliseconds instead of previous value.

--- src/components/PomoBrandHeader.js
+++ src/components/PomoBrandHeader.js
@@ -1,18 +1,25 @@
 import logo from "../assets/pomodoro-clock.gif";
 import FullScreenSection from "../containers/FullScreenSection";
-const PomoBrandHeader = ({pomoBrandHeaderDisplay, setPomoBrandHeaderDisplay}) => {
 
+/**
+ * Functional component that displays a logo and a heading. It uses a timeout to hide the header after a certain duration.
+ *
+ * @param {boolean} pomoBrandHeaderDisplay - A boolean value indicating whether the header should be displayed or not.
+ * @param {function} setPomoBrandHeaderDisplay - A function to update the value of pomoBrandHeaderDisplay.
+ * @returns {JSX.Element} - The rendered logo and heading wrapped in a FullScreenSection component if pomoBrandHeaderDisplay is true.
+ */
+
+const PomoBrandHeader = ({ pomoBrandHeaderDisplay, setPomoBrandHeaderDisplay }) => {
   setTimeout(() => {
     setPomoBrandHeaderDisplay(false);
   }, 2550);
+
   return (
     <>
       {pomoBrandHeaderDisplay && (
         <FullScreenSection extraclasses="flex flex-col justify-center items-center">
-          <img src={logo} alt="Pomo Logo" className=" animate-bounce" />
-          <h1 className="text-5xl text-center font-gilroylight font-bold ">
-            POMO
-          </h1>
+          <img src={logo} alt="Pomo Logo" className="animate-bounce" />
+          <h1 className="text-5xl text-center font-gilroylight font-bold">POMO</h1>
         </FullScreenSection>
       )}
     </>
