011c28a00f6de33c66244e97bd8e530b186adab3
Sushant Kadam <sushantkadam15@gmail.com>
2023-10-13 23:46:05 -0400

    [FullScreenSection] Improve component documentation and add prop types validation. (Refactoring)

--- src/containers/FullScreenSection.js
+++ src/containers/FullScreenSection.js
@@ -1,4 +1,11 @@
-// FullScreenSection is used to control the height and width of all the views centrally
+/**
+ * FullScreenSection is a functional component used to control the height and width of all the views centrally.
+ *
+ * @param {Object} props - The props object.
+ * @param {ReactNode} props.children - The content to be rendered inside the FullScreenSection component.
+ * @param {string} [props.extraclasses] - Additional CSS classes to be applied to the section element.
+ * @returns {ReactElement} The FullScreenSection component.
+ */
 const FullScreenSection = ({ children, extraclasses }) => {
   const globalClasses = "h-screen max-h-screen";
   const combinedClasses = `${globalClasses} ${extraclasses || ""}`;
